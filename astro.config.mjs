141448ea8cef5df8ca00a415b638905a1bb8225d
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-02-07 19:13:43 +0100

    Maxi Ferreira template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.buildsystems.de',
+  integrations: [mdx(), sitemap(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord'
+    }
+  }
 });
