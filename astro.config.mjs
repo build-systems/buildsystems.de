da47f0831e4e1b582be476a6c380cbd5f197eb42
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-02-07 20:46:36 +0100

    Added Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import remarkGfm from 'remark-gfm';
+import remarkSmartypants from 'remark-smartypants';
+import rehypeExternalLinks from 'rehype-external-links';
 
-import svelte from "@astrojs/svelte";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.buildsystems.de',
-  integrations: [mdx(), sitemap(), svelte()],
+  site: 'https://blog.buildsystems.de/',
+  integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
       theme: 'nord'
-    }
-  }
+    },
+    remarkPlugins: [remarkGfm, remarkSmartypants],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank'
+    }]]
+  },
+  output: "server",
+  adapter: netlify()
 });
