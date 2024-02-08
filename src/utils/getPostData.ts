a8c9321d86aa3982c648bb588b67c6a031879205
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-02-08 20:14:13 +0100

    Commented svelte stuff

--- src/utils/getPostData.ts
+++ src/utils/getPostData.ts
@@ -1,11 +1,3 @@
-import readingTime from 'reading-time'
-
-type Post = {
-  title: string
-  file: string
-  rawContent: () => string
-}
-
 export function formatDate(date: Date): string{
   const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'long', day: 'numeric'};
 
