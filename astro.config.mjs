1f400008576b7abe4bce5aa766aaca2c30c9bcaa
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-02-13 15:59:53 +0100

    Disabled other tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import remarkGfm from 'remark-gfm';
-import remarkSmartypants from 'remark-smartypants';
-import rehypeExternalLinks from 'rehype-external-links';
+// import mdx from '@astrojs/mdx';
+// import remarkGfm from 'remark-gfm';
+// import remarkSmartypants from 'remark-smartypants';
+// import rehypeExternalLinks from 'rehype-external-links';
 import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.buildsystems.de/',
-  integrations: [mdx(), lottie()],
-  markdown: {
-    shikiConfig: {
-      theme: 'nord'
-    },
-    remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: '_blank'
-    }]]
-  },
+  integrations: [lottie()],
+  // integrations: [mdx(), lottie()],
+  // markdown: {
+  //   shikiConfig: {
+  //     theme: 'nord'
+  //   },
+  //   remarkPlugins: [remarkGfm, remarkSmartypants],
+  //   rehypePlugins: [[rehypeExternalLinks, {
+  //     target: '_blank'
+  //   }]]
+  // },
 });
