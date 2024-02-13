713a52f7bc9278fe7d65144a53ca92488c691e68
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-02-13 16:36:15 +0100

    Workaround for ViewTransitions bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
 import { defineConfig } from 'astro/config';
-// import mdx from '@astrojs/mdx';
-// import remarkGfm from 'remark-gfm';
-// import remarkSmartypants from 'remark-smartypants';
-// import rehypeExternalLinks from 'rehype-external-links';
+import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
+import remarkSmartypants from 'remark-smartypants';
+import rehypeExternalLinks from 'rehype-external-links';
 import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.buildsystems.de/',
-  integrations: [lottie()],
-  // integrations: [mdx(), lottie()],
-  // markdown: {
-  //   shikiConfig: {
-  //     theme: 'nord'
-  //   },
-  //   remarkPlugins: [remarkGfm, remarkSmartypants],
-  //   rehypePlugins: [[rehypeExternalLinks, {
-  //     target: '_blank'
-  //   }]]
-  // },
+  integrations: [mdx(), lottie()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord'
+    },
+    remarkPlugins: [remarkGfm, remarkSmartypants],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank'
+    }]]
+  },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="astro-integration-lottie/env" />
+declare global {
+    function loadLottie(id?: string): Promise<void>;
+  }
+  
+  export {};
