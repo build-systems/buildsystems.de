98566cf8b364a8bec64255600aeed610d8027499
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-04-10 17:39:47 +0200

    Test again

--- src/integrations/public-notion-copier.ts
+++ src/integrations/public-notion-copier.ts
@@ -6,7 +6,10 @@ export default (): AstroIntegration => ({
   name: "public-notion-copier",
   hooks: {
     "astro:build:done": async ({ dir }) => {
-      const outDir = new URL("notion", dir.href).pathname;
+      // const outDir = new URL("notion", dir.href).pathname;
+      const outDir = "notion";
+      // console.log("dir.href is = " + dir.href);
+      // console.log("outDir is = " + outDir.split(":/").slice(-1)[0]);
       if (!fs.existsSync(outDir)) {
         fs.mkdirSync(outDir);
       }
