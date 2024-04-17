5fa7ce19c9e75afbda7cc7a3e01358cbbe1f761d
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-04-17 11:48:30 +0200

    Included feed and robots files

--- src/pages/api/notionPosts.json.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-import { getAllPosts } from "../../lib/notion/client";
-import type { Post } from "../../lib/interfaces";
-
-export const GET: APIRoute = async (context) => {
-  // console.log("Context", context);
-  const posts = await getAllPosts();
-  return new Response(JSON.stringify(posts));
-};
