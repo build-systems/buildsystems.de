f481d01036d0ea74072170856720218725cdc832
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-04-17 18:04:20 +0200

    Adjusting mobile version

--- src/utils/getPostData.ts
+++ src/utils/getPostData.ts
@@ -5,7 +5,7 @@ export function formatDate(date: Date): string {
   };
   // const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'long', day: 'numeric'};
 
-  return new Date(date).toLocaleDateString(undefined, options);
+  return new Date(date).toLocaleDateString("de-DE", options);
 }
 
 export async function fetchPostsEndpoint() {
