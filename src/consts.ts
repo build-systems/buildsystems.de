6e821f3ec902f8b619178e442d0f075831ada783
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-04-21 16:07:49 +0200

    Changed head properties

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'BuildSystems';
-export const SITE_DESCRIPTION = 'BuildSystems ermöglicht es, den Nachhaltigkeits­anforderungen von morgen ökonomisch und technisch gerecht zu werden. Wir sind eine technologie­gestützte Nachhaltigkeits­beratung. Wir stellen sicher, dass Ihre Projekt­entwicklungen grün, zukunfts­fähig sowie kalkulierbar und risikoarm sind.';
-export const SITE_HEROIMAGE = '/images/multistory-timber-building-lowres.jpg';
+export const SITE_TITLE = "BuildSystems";
+export const SITE_DESCRIPTION =
+  "BuildSystems ermöglicht es, den Nachhaltigkeits­anforderungen von morgen ökonomisch und technisch gerecht zu werden. Wir sind eine technologie­gestützte Nachhaltigkeits­beratung. Wir stellen sicher, dass Ihre Projekt­entwicklungen grün, zukunfts­fähig sowie kalkulierbar und risikoarm sind.";
+export const SITE_HEROIMAGE = "/images/multistory-timber-building-lowres.jpg";
+export const SITE_HEROIMAGEALT =
+  "Multistory Timber Building with a lively neighborhood";
