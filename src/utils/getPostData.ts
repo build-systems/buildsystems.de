55404b26225bf254d1490757fb438ddd75ba383b
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-04-27 20:14:30 +0200

    Deleted fetchPostsEndpoint(). It  seems to be an old test

--- src/utils/getPostData.ts
+++ src/utils/getPostData.ts
@@ -7,24 +7,3 @@ export function formatDate(date: Date): string {
 
   return new Date(date).toLocaleDateString("de-DE", options);
 }
-
-export async function fetchPostsEndpoint() {
-  console.log("\n===== Endpoint function =====");
-  let DATA;
-  if (!DATA) {
-    try {
-      // console.log(Astro.site + "api/notionPosts.json");
-      const res = await fetch(Astro.site + "api/notionPosts.json");
-      console.dir(res);
-      if (!res.ok) {
-        throw new Error("Response from posts endpoint not ok 03");
-      }
-      const data = await res.json();
-      DATA = data;
-      return data;
-    } catch (e) {
-      console.log("Error getting data from posts endpoint 03");
-      console.error(e);
-    }
-  }
-}
