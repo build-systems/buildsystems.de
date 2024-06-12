623e4488c49ead5b026798a20764f23b318aeb41
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-06-12 18:13:12 +0200

    Just build to download the new images

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
+import type { BundledLanguage } from "../../node_modules/shiki/dist/langs.d.mts";
+
+type SpecialLanguage = "text" | "txt" | "ansi";
+
 export const pathJoin = (path: string, subPath: string) => {
   return (
     "/" +
@@ -8,3 +12,35 @@ export const pathJoin = (path: string, subPath: string) => {
       .join("/")
   );
 };
+
+export function notionLangToShiki(
+  lang: any
+): BundledLanguage | SpecialLanguage {
+  switch (lang) {
+    case "arduino":
+      return "cpp";
+    case "flow":
+      return "javascript";
+    case "fortran":
+      return "fortran-fixed-form";
+    case "livescript":
+      return "javascript";
+    case "markup":
+      return "markdown";
+    case "plain text":
+      return "text";
+    case "protobuf":
+      return "proto";
+    case "reason":
+      return "javascript";
+    case "vb.net":
+      return "vb";
+    case "visual basic":
+      return "vb";
+    case "webassembly":
+      return "wasm";
+    case "java/c/c++/c#":
+      return "c";
+  }
+  return lang;
+}
